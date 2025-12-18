f727053e83a0bdf31a2131d04f58d7325a02eef7
doru <doru.moraru@icloud.com>
2025-12-18 22:02:48 +0000

    Update to v2.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.github.dorumrr.privacyflip"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19
-        versionName = "2.0.8"
+        versionCode = 20
+        versionName = "2.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

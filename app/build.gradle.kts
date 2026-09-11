464d7c210321d448f280319ff93a86f01c20d034
doru <doru.moraru@icloud.com>
2026-09-11 16:36:43 +0100

    Update vto v2.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         minSdk = 24
         targetSdk = 35
 
-        versionCode = 27
-        versionName = "2.1.6"
+        versionCode = 28
+        versionName = "2.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
